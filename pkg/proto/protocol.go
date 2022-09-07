6db6c6139b25ca3933bcbd0f2e67d409a88e2323
Snavy <1337Snavy@gmail.com>
2022-09-07 22:38:09 +0200

    added methods to protocol messages

--- pkg/proto/protocol.go
+++ pkg/proto/protocol.go
@@ -14,3 +14,39 @@ type ExecuteResult struct {
 	Payload map[string]interface{} `json:"pl"`
 	Error   string                 `json:"er,omitempty"`
 }
+
+func (cmd *ExecuteCommand) SetNamespace(value string) *ExecuteCommand {
+	cmd.Namespace = value
+	return cmd
+}
+
+func (cmd *ExecuteCommand) SetMethod(value string) *ExecuteCommand {
+	cmd.Method = value
+	return cmd
+}
+
+func (cmd *ExecuteCommand) SetParams(value map[string]interface{}) *ExecuteCommand {
+	cmd.Params = value
+	return cmd
+}
+
+func (cmd *ExecuteCommand) SetParam(key string, value interface{}) *ExecuteCommand {
+	if cmd.Params == nil {
+		cmd.Params = map[string]interface{}{}
+	}
+	cmd.Params[key] = value
+	return cmd
+}
+
+func (res *ExecuteResult) ToPayload(value map[string]interface{}) *ExecuteResult {
+	res.Success = true
+	res.Payload = value
+	return res
+}
+
+func (res *ExecuteResult) ToError(value string) *ExecuteResult {
+	res.Success = false
+	res.Payload = nil
+	res.Error = value
+	return res
+}
